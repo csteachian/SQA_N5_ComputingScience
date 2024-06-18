2695dd1eaaaf3746819f7e90a40e17cb564ff6b9
Ian Simpson <ian@csteach.uk>
2024-06-18 14:23:10 +0100

    Added variables-recap.py

---
+++ Week 2/variables-recap.py
@@ -0,0 +1,11 @@
+# I Simpson
+# Set up 5 variables
+firstName = "Ian" # string
+surname = "Simpson" # string
+age = 72 # integer
+initial = "D" # character
+takesComputing = True # boolean
+
+# Use these 5 variables in a sentence e.g
+# 'Ian D Simpson is 72 years old and takes computing'
+print(firstName,initial,surname)
